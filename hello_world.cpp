57b157ef285be0e73cfecf26b1cb99471be59d13
1Velvet1 <89218548+1Velvet1@users.noreply.github.com>
2022-04-11 23:00:59 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,11 +4,19 @@ int
 main(int agc, string* argv[])
 {
 
+<<<<<<< HEAD
   std::string name; // for storing user name.
 
   std::cin >> name; // getting user name.
 
   std::cout << "Hello, World from " + name + "!\n"; // output.
+=======
+     std::string name;  //для хранения имени пользователя
+
+     std::cin >> name;  //получения имени
+
+     std::cout << "Hello, World from " + name + "!\n"; //вывод
+>>>>>>> Update hello_world.cpp
 
   return 0;
 
