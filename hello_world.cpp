3e7a6b055905392d8b846495d3ed94d8011be33e
1Velvet1 <levo.bond@gmail.com>
2022-04-11 22:54:21 +0300

    formatted with clang

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
-int main(int agc, string* argv[]){
+int
+main(int agc, string* argv[])
+{
 
-     std::string name;  //for storing user name
+  std::string name; // for storing user name
 
-     std::cin >> name;  //getting user name
+  std::cin >> name; // getting user name
 
-     std::cout << "Hello, World from " + name + "!\n"; //output
+  std::cout << "Hello, World from " + name + "!\n"; // output
 
-    return 0;  
-
+  return 0;
 }
